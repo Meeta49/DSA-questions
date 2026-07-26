b04f4001d249f44d6560e1803e527000bb690d8a
Meeta Parsad <151948122+Meeta49@users.noreply.github.com>
2026-07-27 00:04:36 +0530

    Time: 6 ms (5.82%) | Memory: 17.2 MB (16.27%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -14,36 +14,47 @@ class Solution
 public:
     vector<vector<int>> levelOrder(TreeNode* root)
     {
-        vector<vector<int>> ans;
+        vector<vector<int>> ans;        //store all the levels
 
-        if(root==NULL)
+        if(root==NULL)       //check the empty tree and return the ans.
             return ans;
 
-        queue<TreeNode*> q;
-        q.push(root);
+        queue<TreeNode*> q;           //use the queue because ot traverse the level by level
+        q.push(root);           //start the traversal from the root node
 
-        while(!q.empty())
+        while(!q.empty())       //whenever queue will not empty it will run
         {
-            int size=q.size();
-            vector<int> level;
+            int size=q.size();       //count the current level node
+            vector<int> level;      //store the current level node
 
-            for(int i=0;i<size;i++)
+            for(int i=0;i<size;i++)       //process the node for current level
             {
-                TreeNode* node=q.front();
-                q.pop();
+                TreeNode* node=q.front(); // take the front node as a queue
+                q.pop();                   // remove that node from the queue
 
-                level.push_back(node->val);
+                level.push_back(node->val); //store the node value into the current level
 
-                if(node->left)
+                if(node->left)           //if left child exist add into the queue
                     q.push(node->left);
 
-                if(node->right)
+                if(node->right)                  //if the right child exist add into the queue
                     q.push(node->right);
             }
 
-            ans.push_back(level);
+            ans.push_back(level);     //all the current level process and add into the final ans
         }
 
-        return ans;
+        return ans;  //return all the final ans.
     }
 };
+
+
+// 1. Queue me Root daalo.
+// 2. Jab tak Queue empty na ho:
+//    - Current level ka size nikalo.
+//    - Size jitni baar loop chalao.
+//    - Front node nikalo.
+//    - Answer me add karo.
+//    - Left aur Right child ko Queue me daalo.
+//    - Current level ko final answer me store karo.
+// 3. Answer return karo.
