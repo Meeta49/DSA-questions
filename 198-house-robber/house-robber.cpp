7191fbe86490c435f0d5f977f2cde1ed6f082352
Meeta Parsad <151948122+Meeta49@users.noreply.github.com>
2026-07-30 09:50:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (47.28%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -17,3 +17,18 @@ public:
         return dp[ind]=max(pick,notPick);
     }
 };
+
+// class Solution 
+// {
+// public:
+//     int rec(vector<int>& nums,int idx)
+//     {
+//         if(idx>=nums.size())
+//         return 0;
+//         return max(nums[idx]+rec(nums,idx+2),rec(nums,idx+1));
+//     }
+//     int rob(vector<int>& nums) 
+//     {
+//         return rec(nums,0);
+//     }
+// };
